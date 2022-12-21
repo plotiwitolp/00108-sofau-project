9fc18471667895ff34eb97d141165c841b11d3a5
plotiwitolp <plotiwitolp@gmail.com>
2022-12-21 22:22:19 +0400

    update

--- index.php
+++ index.php
@@ -1,17 +1,146 @@
 <!DOCTYPE html>
-<html lang="ru">
+<html xml:lang="en-gb" lang="en-gb">
 
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Wellore: Game art and development outsourcing studio | 2D and 3D art for games</title>
+    <meta name="keywords" content="gamedev, game art, game design, 2d art, 3d art, character design, environment design, props, vfx, animation, game development, illustration">
+    <meta name="description" content="Game art and Development outsourcing studio. We have been creating 2D and 3D art for more than 12 years. We turn your ideas into reality!">
+    <link rel="stylesheet" href="./assets/fonts/Lato/stylesheet.css">
     <link rel="stylesheet" href="./assets/styles/style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
 </head>
 
 <body>
+    <header>
+        <div class="section">
+            <div class="header">
+                <div class="header-left-top">
+                    <div class="header__logo">
+                        <img src="./assets/images/logo.svg" alt="wellore.com">
+                    </div>
+                    <nav class="header__desk-wrap">
+                        <ul class="header__desk">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a>
+                                <ul class="header__desk-sub">
+                                    <li><a href="#">3D Art</a></li>
+                                    <li><a href="#">2D Art</a></li>
+                                    <li><a href="#">Animation</a></li>
+                                    <li><a href="#">Game development</a></li>
+                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Cinematics</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Portfolio</a>
+                                <ul class="header__desk-sub">
+                                    <li><a href="#">3D Art</a></li>
+                                    <li><a href="#">2D Art</a></li>
+                                    <li><a href="#">Animation</a></li>
+                                    <li><a href="#">Game development</a></li>
+                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Cinematics</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                    <div class="nav-burger">
+                        <span></span>
+                        <span></span>
+                        <span></span>
+                    </div>
+                </div>
+                <div class="header-right-bottom">
+                    <nav class="header__mob-wrap">
+                        <ul class="header__mob">
+                            <li><a href="#">Home</a></li>
+                            <li><a href="#">Services</a>
+                                <ul class="header__mob-sub">
+                                    <li><a href="#">3D Art</a></li>
+                                    <li><a href="#">2D Art</a></li>
+                                    <li><a href="#">Animation</a></li>
+                                    <li><a href="#">Game development</a></li>
+                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Cinematics</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">Portfolio</a>
+                                <ul class="header__mob-sub">
+                                    <li><a href="#">3D Art</a></li>
+                                    <li><a href="#">2D Art</a></li>
+                                    <li><a href="#">Animation</a></li>
+                                    <li><a href="#">Game development</a></li>
+                                    <li><a href="#">Marketing</a></li>
+                                    <li><a href="#">Cinematics</a></li>
+                                </ul>
+                            </li>
+                            <li><a href="#">About us</a></li>
+                            <li><a href="#">Blog</a></li>
+                            <li><a href="#">Contacts</a></li>
+                        </ul>
+                    </nav>
+                    <div class="button header__contact-us">
+                        <a href="#">Contact Us</a>
+                    </div>
+                    <div class="header__socials">
+                        <div class="socials">
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/mail.svg" alt="mail wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/facebook.svg" alt="facebook wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/instagram.svg" alt="instagram wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/pinterest.svg" alt="pinterest wellore.com">
+                                </a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
+    <main>
+        <div class="section">
+            <section>
+                <div class="main-pic">
+                    <img src="./assets/images/baner-1.png" alt="baner-1">
+                    <span class="main-pic__bg">
+                        <img src="./assets/images/baner-1-bg.png" alt="baner-1-bg">
+                    </span>
+                </div>
+                <div class="main-title">
+                    <h1><span class="title-accent">Development</span> and <span class="title-accent">game art</span>outsourtcing studio.</h1>
+                    <div class="description">
+                        We have been creating 2D and 3D art for more than 12 years. We turn your ideas into reality!
+                    </div>
+                    <div class="button main-title__contact-us">
+                        <a href="#">Contact Us</a>
+                    </div>
+                </div>
+            </section>
+        </div>
+    </main>
+    <footer>
+        <div class="section">
 
+        </div>
+    </footer>
     <script src="./assets/scripts/jquery-3.6.0.min.js"></script>
     <script src="./assets/scripts/script.js"></script>
 </body>
