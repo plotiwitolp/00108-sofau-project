0ce8f97b89d9fc73c38eaba8fc6971a98a17c7c1
plotiwitolp <plotiwitolp@gmail.com>
2022-12-21 15:45:49 +0400

    init

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./assets/styles/style.css">
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+</head>
+
+<body>
+
+    <script src="./assets/scripts/jquery-3.6.0.min.js"></script>
+    <script src="./assets/scripts/script.js"></script>
+</body>
+
+</html>
