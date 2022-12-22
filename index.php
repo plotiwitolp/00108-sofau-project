30c23d405b5e954ecd8747a08d78700987212811
plotiwitolp <plotiwitolp@gmail.com>
2022-12-22 10:32:12 +0400

    markup html home-page

--- index.php
+++ index.php
@@ -117,6 +117,7 @@
     </header>
     <main>
         <div class="section">
+            <!-- TOP -->
             <section>
                 <div class="main-pic">
                     <img src="./assets/images/baner-1.png" alt="baner-1">
@@ -134,11 +135,331 @@
                     </div>
                 </div>
             </section>
+            <!-- OUR SERVIСES -->
+            <section>
+                <h2>OUR SERVIСES</h2>
+                <div class="job-preview">
+                    <div class="job-preview__item">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/our_services-3d_art.png" alt="our_services-3d_art">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>3D art</h3>
+                            <p>
+                                We create all kinds of 3D models: characters, creatures, locations, buildings, vehicles and weapons. We use Low-poly, High-poly modeling, PBR texturing, 3D sculpting, and also prepare assets for Unreal Engine, Unity 3D, CryEngine.
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                    <div class="job-preview__item">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/our_services-2d_art.png" alt="our_services-2d_art">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>2D art</h3>
+                            <p>
+                                Our team develops locations, illustrations, characters, game objects and icons. We will create a new style or we will adjust to the features of the finished game project.
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                    <div class="job-preview__item">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/animation.png" alt="animation">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>Animation</h3>
+                            <p>
+                                We create realistic animation and bright special effects. Our team will revive your 2D and 3D projects!
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                    <div class="job-preview__item">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/game_development.png" alt="game_development">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>Game development</h3>
+                            <p>
+                                We make exciting and realistic trailers and high quality in-game videos.
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                    <div class="job-preview__item job-preview__item__active">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/marketing.png" alt="marketing">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>Marketing</h3>
+                            <p>
+                                Marketing research, launching advertising campaigns and attracting traffic, organizing SMM and community management for your game.
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                    <div class="job-preview__item">
+                        <div class="job-preview__thumb">
+                            <img src="./assets/images/cinematics.png" alt="cinematics">
+                        </div>
+                        <div class="job-preview__desc">
+                            <h3>Cinematics</h3>
+                            <p>
+                                Our team has been creating addictive gameplay and immersive game environments for over 10 years.
+                            </p>
+                            <span class="see-more-btn">See more</span>
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- Our advantages -->
+            <section>
+                <h2>Our advantages</h2>
+                <div class="advantages">
+                    <div class="advantages__item">
+                        <div class="advantages__icon">
+                            <img src="./assets/images/dawn.svg" alt="dawn">
+                        </div>
+                        <div class="advantages__body">
+                            <span>Individual approach</span>
+                            <p>
+                                We will take into account all your wishes when creating a project.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="advantages__item">
+                        <div class="advantages__icon">
+                            <img src="./assets/images/dawn.svg" alt="dawn">
+                        </div>
+                        <div class="advantages__body">
+                            <span>Professionalism and quality</span>
+                            <p>
+                                Our team of artists, developers, designers and managers has a wealth of experience and will help to implement all your ideas.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="advantages__item">
+                        <div class="advantages__icon">
+                            <img src="./assets/images/dawn.svg" alt="dawn">
+                        </div>
+                        <div class="advantages__body">
+                            <span>Complete Solutions</span>
+                            <p>
+                                We offer complex solutions that allow us to solve several problems at once or we can create a full-fledged project from scratch.
+                            </p>
+                        </div>
+                    </div>
+                    <div class="advantages__item">
+                        <div class="advantages__icon">
+                            <img src="./assets/images/dawn.svg" alt="dawn">
+                        </div>
+                        <div class="advantages__body">
+                            <span>High security</span>
+                            <p>
+                                Your data is safe: we strictly adhere to the privacy policy.
+                            </p>
+                        </div>
+                    </div>
+                </div>
+                <div class="technic">
+                    <div class="technic-wrap">
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-1.svg" alt="technic-1">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-2.svg" alt="technic-2">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-3.svg" alt="technic-3">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-4.svg" alt="technic-4">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-5.svg" alt="technic-5">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-6.svg" alt="technic-6">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-7.svg" alt="technic-7">
+                        </div>
+                        <div class="technic__item">
+                            <img src="./assets/images/technic-8.svg" alt="technic-8">
+                        </div>
+                    </div>
+                </div>
+            </section>
+            <!-- OUR WORKS -->
+            <section>
+                <div class="works-title">
+                    <h2>OUR WORKS</h2>
+                    <span class="see-more-arrow">
+                        <span>See more</span>
+                        <img src="./assets/images/see-more-arrow.svg" alt="see-more-arrow">
+                    </span>
+                </div>
+                <div class="works-grid">
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-1.png" alt="our_works-1">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-2.png" alt="our_works-2">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-3.png" alt="our_works-3">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-4.png" alt="our_works-4">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-5.png" alt="our_works-5">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-6.png" alt="our_works-6">
+                    </div>
+                    <div class="works-grid__item">
+                        <img src="./assets/images/our_works-7.png" alt="our_works-7">
+                    </div>
+                </div>
+            </section>
+            <!--  Feedback form -->
+            <section>
+                <div class="feedback-form">
+                    <div class="feedback-form__left">
+                        <div class="feedback-form__top">
+                            <h2>READY TO START YOUR PROJECT?</h2>
+                            <div class="feedback-form__hint">
+                                Leave your contact details. We will get back to you soon.
+                            </div>
+                        </div>
+                        <form class="feedback-form__form" action="POST">
+                            <div class="feedback-form__middle">
+                                <div class="feedback-form__middle-item">
+
+                                    <div class="feedback-form__label">
+                                        <span class="feedback-form__title">Name</span>
+                                        <span class="feedback-form__asterix">*</span>
+                                    </div>
+                                    <div class="feedback-form__input">
+                                        <input type="text" maxlength="20">
+                                    </div>
+                                </div>
+                                <div class="feedback-form__middle-item">
+                                    <div class="feedback-form__label">
+                                        <span class="feedback-form__title">Email</span>
+                                        <span class="feedback-form__asterix">*</span>
+                                    </div>
+                                    <div class="feedback-form__input">
+                                        <input type="mail" maxlength="20">
+                                    </div>
+                                </div>
+                                <div class="feedback-form__middle-item">
+                                    <div class="feedback-form__label">
+                                        <span class="feedback-form__title">Project</span>
+                                        <span class="feedback-form__asterix">*</span>
+                                    </div>
+                                    <div class="feedback-form__input">
+                                        <input type="text" maxlength="20">
+                                    </div>
+                                </div>
+                                <div class="feedback-form__middle-item">
+                                    <div class="feedback-form__label">
+                                        <span class="feedback-form__title">Project description</span>
+                                        <span class="feedback-form__asterix">*</span>
+                                    </div>
+                                    <div class="feedback-form__input">
+                                        <textarea></textarea>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="feedback-form__bottom">
+                                <div class="button feedback-form__send">
+                                    <button type="submit">Send</button>
+                                </div>
+                                <div class="feedback-form-pers-data">
+                                    <input class="feedback-form-pers-data__check" type="checkbox">
+                                    <span class="feedback-form-pers-data__text">
+                                        I agree with <a href="#">processing of my personal data</a>
+                                    </span>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                    <div class="feedback-form__right">
+                        <div class="socials">
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/mail.svg" alt="mail wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/facebook.svg" alt="facebook wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/instagram.svg" alt="instagram wellore.com">
+                                </a>
+                            </div>
+                            <div class="socials-item">
+                                <a href="#">
+                                    <img src="./assets/images/socials/pinterest.svg" alt="pinterest wellore.com">
+                                </a>
+                            </div>
+                        </div>
+                        <img src="./assets/images/wellorepromoscreen_caschar.png" alt="wellorepromoscreen_caschar">
+                    </div>
+                </div>
+            </section>
         </div>
     </main>
     <footer>
+        <div class="footer-section">
+            <div class="section">
+                <nav class="footer-nav">
+                    <ul>
+                        <li class="footer-nav__item">3D Art</li>
+                        <li class="footer-nav__item">2D Art</li>
+                        <li class="footer-nav__item">Animation</li>
+                        <li class="footer-nav__item">Game development</li>
+                        <li class="footer-nav__item">Marketing</li>
+                        <li class="footer-nav__item">Cinematics</li>
+                    </ul>
+                </nav>
+            </div>
+        </div>
         <div class="section">
-
+            <div class="footer-bottom">
+                <div class="footer-bottom__mail">contact@wellore.com</div>
+                <div class="footer-bottom__socials">
+                    <div class="footer-bottom__socials__item">
+                        <a href="#">
+                            <img src="./assets/images/socials/youtube.svg" alt="youtube">
+                        </a>
+                    </div>
+                    <div class="footer-bottom__socials__item">
+                        <a href="#">
+                            <img src="./assets/images/socials/twitter.svg" alt="twitter">
+                        </a>
+                    </div>
+                    <div class="footer-bottom__socials__item">
+                        <a href="#">
+                            <img src="./assets/images/socials/facebook-footer.svg" alt="facebook">
+                        </a>
+                    </div>
+                    <div class="footer-bottom__socials__item">
+                        <a href="#">
+                            <img src="./assets/images/socials/google-plus.svg" alt="google-plus">
+                        </a>
+                    </div>
+                </div>
+            </div>
+            <div class="copyright">
+                All rights reserved @Wellore
+            </div>
         </div>
     </footer>
     <script src="./assets/scripts/jquery-3.6.0.min.js"></script>
